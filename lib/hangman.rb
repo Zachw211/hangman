0e57e26e702d2536b53d5c29611d266358bcdc15
Zach Weinstein <zweinstein@gmail.com>
2015-10-28 17:03:56 -0600

    third commit

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -4,11 +4,64 @@ class Hangman
 		@all_words = File.readlines "5desk.txt"
 		@words = []
 		@secret_word = ""
+		@guess_string = ""
 		@guess_word = []
 		@guess = 0
 		@guessed_letters = []
 		@chances = 10
+		@playing = true
+		@hanged = false
 
+	end
+
+	def game_loop
+		while @playing
+			get_guess
+			check_guess
+			display_guess
+			puts @secret_word
+			game_check
+			game_over	
+		end
+	end
+					
+	def game_check
+		@guess_string = @guess_word.join
+		if @chances == 0
+			@playing = false
+			@hanged = true
+		elsif @guess_string== @secret_word
+			@playing = false
+		end
+	end
+
+	def game_over
+		if !@playing
+			if @hanged
+				lose_message
+			else
+				win_message
+			end
+		end
+	end
+	
+	def lose_message
+		puts "You have been hanged."
+		puts "Play again(y/n)"
+		if gets.chomp == "y"
+			game = Hangman.new
+			game.play
+		end
+
+	end
+
+	def win_message
+		puts "Way to go! You won!"
+		puts "Play again(y/n)"
+		if gets.chomp == "y"
+			game = Hangman.new
+			game.play
+		end
 	end
 
 	def get_words
@@ -21,13 +74,17 @@ class Hangman
 
 	def get_guess
 		puts "Guess a letter."
-		puts "You have already guessed #{@guessed_letters}"
+		puts "You have already guessed: #{@guessed_letters.join(", ")}"
 		@letter = gets.chomp.downcase
+		if @guessed_letters.include?(@letter)
+			puts "You have already guessed this letter. Guess again."
+			get_guess
+		end
 		@guessed_letters << @letter
 	end
 
 	def get_secret_word
-		@secret_word = @words[rand(1..48891)]
+		@secret_word = @words[rand(1..48891)].downcase
 	end
 
 	def	create_guess
@@ -49,22 +106,26 @@ class Hangman
 			puts "I'm sorry, that letter is not in the secret word."
 			puts "You have #{@chances} guesses left."
 		end
-		
 	end
 
-	def test_functions
+	def display_guess
+		puts @guess_word.join(" ")
+
+	end
+
+	def play
 		get_words
 		get_secret_word
 		puts @secret_word
 		create_guess
-		puts @guess_word.join(" ")
-		get_guess
-		check_guess
-		puts @guess_word.join(" ")
+		display_guess
+		game_loop
+
 	end
+
 
 
 end
 
 test = Hangman.new
-test.test_functions
+test.play
